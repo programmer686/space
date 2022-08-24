e673331ed3aa0cbed6c1977e3c6f0eb4eb190778
programmer686 <105795650+programmer686@users.noreply.github.com>
2022-08-23 21:26:59 -0500

    my changes

---
+++ components/TechCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from "../styles/TechCard.module.css"
+function TechCard(props) {
+    function hanldeSwitch(id) {
+        if (id === "LAUNCH") {
+            props.state(0)
+        }
+        else if (id === "SPACEPORT") {
+            props.state(1)
+        }
+        else if (id === "CAPSULE") {
+            props.state(2)
+        }
+    }
+  return (
+    <div className={styles.techContainer}>
+        <div className={styles.btnAndInfoContainer}>
+        <div className={styles.btnContainer}>
+            <button onClick={() => hanldeSwitch("LAUNCH")} className={styles.btns}>1</button>
+            <button onClick={() => hanldeSwitch("SPACEPORT")} className={styles.btns}>2</button>
+            <button onClick={() => hanldeSwitch("CAPSULE")} className={styles.btns}>3</button>
+        </div>
+        <div className={styles.infoBody}>
+            <p className={styles.terminology}>THE TERMINOLOGY...</p>
+            <h1 className={styles.name}>{props.name}</h1>
+            <p className={styles.description}>{props.description}</p>
+        </div>
+        </div>
+        <img className={styles.image} src={props.image} alt="" />
+    </div>
+  )
+}
+
+export default TechCard
--- pages/Technology.js
+++ pages/Technology.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "../styles/Technology.module.css"
+import techData from '../public/data/techData'
+import TechCard from '../components/TechCard'
 function Technology() {
+const [switching, setSwitching] = useState(0)
+  const techCard = techData.map(item => {
+    return (
+      <TechCard 
+      key={item.key}
+      id={item.id}
+      name={item.name}
+      description={item.description}
+      image={item.image}
+      state={setSwitching}
+      />
+    )
+  })
+
   return (
     <div className={styles.body}>
-
+      {techCard[switching]}
     </div>
   )
 }
